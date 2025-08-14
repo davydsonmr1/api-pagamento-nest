aad809d3d9883ffc282e970118047368e66875de
Davydson Maciel <davydsonmaciel.dev@gmail.com>
2025-08-14 13:38:38 -0300

    instalando modulos de pagamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
+import { PagamentoModule } from './pagamento/pagamento.module';
 
 @Module({
-  imports: [],
+  imports: [PagamentoModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
