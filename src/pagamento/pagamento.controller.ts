54e2969f00fa8a42671dfedef9a1c7bf84551675
hdldev123 <hdlgithub@gmail.com>
2025-08-18 16:00:04 -0300

    feat: adiciona endpoints para adicionar valor e consultar saldo

--- src/pagamento/pagamento.controller.ts
+++ src/pagamento/pagamento.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { PagamentoService } from './pagamento.service';
+
 
 @Controller('/pagamento')
 export class PagamentoController {
   constructor(private readonly pagamentoService: PagamentoService) {}
+
+  @Post('adicionar')
+  adicionarValor(@Body('valor') valor: number) {
+    const saldo = this.pagamentoService.adicionarValor(valor);
+    return { saldo }; // retornando o saldo pra testar
+  }
+
+  @Get('saldo')
+  obterSaldo() {
+    const saldo = this.pagamentoService.obterSaldo();
+    return { saldo }; // pega o saldo 
+  }
 
   @Get()
   check() {
@@ -12,5 +26,4 @@ export class PagamentoController {
       timestamp: new Date().toISOString()
     };
   }
-  
 }
--- src/pagamento/pagamento.service.ts
+++ src/pagamento/pagamento.service.ts
@@ -1,4 +1,16 @@
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PagamentoService {}
+export class PagamentoService {
+	private saldo: number = 0;
+
+	adicionarValor(valor: number): number {
+		this.saldo += valor;
+		return this.saldo;
+	}
+
+	obterSaldo(): number {
+		return this.saldo;
+	}
+}
