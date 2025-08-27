65c382f863c0a16cb31aec0408a4c91b9fcf15b8
Davydson Maciel Rafael <davydson.rafael@energisa.com.br>
2025-08-27 17:20:17 -0300

    Configurando o controller da auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('v1/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() body: any) {
+    const user = await this.authService.validateUser(body.usuario, body.password);
+    if (user) {
+      return this.authService.login(user);
+    }
+    return {
+      statusCode: HttpStatus.UNAUTHORIZED,
+      message: 'Credenciais inválidas',
+    };
+  }
+}
