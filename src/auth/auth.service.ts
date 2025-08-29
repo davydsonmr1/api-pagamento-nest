aaa36903f5db2d7498ee29a43fe59c724552c8f8
davydsonmr1 <davydsonmaciel.dev@gmail.com>
2025-08-29 16:51:06 -0300

    desafio sexta concluido data: sexta 16:51

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,19 +5,18 @@ import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
-  async validateUser(username: string, password: string): Promise<any> {
-
+  validateUser(username: string, password: string): boolean {
     const user = { username: 'teste', password: 'teste' };
-    if (user.username === username && user.password === password) {
-      const { password, ...result } = user;
-      return result;
+    const userId = 1; 
+      if (user.username === username && user.password === password) {
+      return true;
     }
-    return null;
+    return false;
   }
 
   //  gerar o token JWT.
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.username , sub: 1 };
     return {
       token: this.jwtService.sign(payload),
     };
